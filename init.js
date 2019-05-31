bbcc35548e9977e676d4df145c7d572075653e21
LrE <lrespino@gmail.com>
2019-05-31 04:28:10 -0400

    js click function update2

--- init.js
+++ init.js
@@ -1,24 +1,24 @@
  // Initial array of gif
- var gifs = [" Altitude ", 
- " Alien vs Ninja ", 
- " Arctic Blast ", 
- " Batman: Under the Red Hood ", 
- " Beyond the Black Rainbow ", 
- " The Book of Eli ", 
- " The Crazies ", 
- " Daybreakers ", 
- " Denizen ", 
- " Despicable Me ", 
- " The Disappearance of Haruhi Suzumiya ", 
- " Downstream ", 
- " Enthiran ", 
- " Future X-Cops ", 
- " Growth ", 
- " Hot Tub Time Machine ", 
- " Hunter Prey ", 
- " Inception ", 
- " Iron Man 2 ", 
- " Justice League: Crisis on Two Earths "];
+ var gifs = ["Altitude", 
+ "Alien vs Ninja", 
+ "Arctic Blast", 
+ "Batman: Under the Red Hood", 
+ "Beyond the Black Rainbow", 
+ "The Book of Eli", 
+ "The Crazies", 
+ "Daybreakers", 
+ "Denizen", 
+ "Despicable Me", 
+ "The Disappearance of Haruhi Suzumiya", 
+ "Downstream", 
+ "Enthiran", 
+ "Future X-Cops", 
+ "Growth", 
+ "Hot Tub Time Machine", 
+ "Hunter Prey", 
+ "Inception", 
+ "Iron Man 2", 
+ "Justice League: Crisis on Two Earths"];
  function alertGifName() {
 
 }
@@ -37,8 +37,28 @@
     event.preventDefault();
     console.log(this);
     var gif = $("#gif-input").val().trim();
-    movies.push(gif);
+    gifs.push(gif);
     renderButtons();
   });
   $(document).on("click", ".gif-btn", alertGifName);
   renderButtons();
+
+  function alertGifName() {
+//create var for g rated gif
+// creat var for number of result return
+    var gifName = $(this).attr("data-name");
+    var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + gifName + "&api_key=DoHLXkG59mk873eFXDMLJERy2NX0xJYr";
+  console.log(queryURL)
+    // Creates AJAX call for the specific movie gif being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(queryURL);
+        console.log(response);
+//append 20 result
+//append rating
+
+    });
+
+}
